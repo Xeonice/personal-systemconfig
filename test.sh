3284d025d82249353a9d898885db2b4413adacfc
DouglasDong <ad546971975@gmail.com>
2020-01-31 11:07:21 +0800

    [IMP]添加线上拉取功能

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
-echo "----------------更新系统---------------"
-sudo apt-get update
-sudo apt-get upgrade
-echo "----------------安装ZSH、git、wget-----"
-sudo apt-get install zsh git wget
-echo "----------------安装oh-my-zsh----------"
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# #!/bin/sh
+# echo "----------------更新系统---------------"
+# sudo apt-get update
+# sudo apt-get upgrade
+# echo "----------------安装ZSH、git、wget-----"
+# sudo apt-get install zsh git wget
+# echo "----------------安装oh-my-zsh----------"
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "----------------安装zsh插件------------"
 # syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 echo "----------------拉取线上的.zshrc-------"
-wget url -O .zshrc
+wget --no-check-certificate -O ~/.zshrc https://raw.githubusercontent.com/Xeonice/personal-systemconfig/master/.zshrc?token=ADGILQTSBNX5SJ6A3KPH5JK6GOMZA
